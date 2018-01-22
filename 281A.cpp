e104ef1bc1a9527dbbc4c9a20c8f902cce35f028
Naveen Chopra <31899453+naveenchopra9@users.noreply.github.com>
2018-01-22 22:43:28 +0530

    Create 281A.cpp

---
+++ 281A.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <cstring>
+#include <map>
+#include <vector>
+#include <cstring>
+#include <regex>
+using namespace std;
+#define IN ({int n; scanf("%d", &n);    n;})
+#define CN ({char _char; scanf(" %c",&_char); _char;})a
+#define REP(i, a, b) for (int i = int(a); i < int(b); i++)
+#define FR freopen("input.txt","r",stdin)
+#define FW freopen("output.txt","w",stdout)
+int main()
+{
+//    int  n;
+    string str;
+   // cin>>n;
+    cin>>str;
+    str[0]=toupper(str[0]);
+    cout<<str<<endl;
+}
+
